1bf34d82725c6ab631b80b73bb215e91595260c8
ladanziaee@gmail.com <ladanziaee@gmail.com>
2018-02-21 23:19:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 var name = "Joe"
 var height = "74"
-if (name==="susan")
-{
-  console.log("the name is correct")
-}
-else { (name==="susan")
-console.log("expected the name to b
 // Don't worry about this
 module.exports = { name, height
 }
